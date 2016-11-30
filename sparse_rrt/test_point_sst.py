b5c92d7f54e2bb8491cd5ccd8c037e631a806d59
olegsinyavskiy <olegsinyavskiy@gmail.com>
2016-11-29 22:07:19 -0800

    constructing

--- sparse_rrt/test_point_sst.py
+++ sparse_rrt/test_point_sst.py
@@ -4,8 +4,22 @@ import _sst_module
 
 def test_point_sst():
     point = _sst_module.Point()
-    
-    sst = _sst_module.SSTWrapper()
+
+    # The radius for BestNear (delta_n in the WAFR paper)
+    sst_delta_near=.4
+    # The radius for sparsification (delta_s in the WAFR paper)
+    st_delta_drain=.2
+
+    sst = _sst_module.SSTWrapper(
+        point.get_state_bounds(),
+        point.get_control_bounds(),
+        point.is_circular_topology(),
+        0,
+        sst_delta_near,
+        st_delta_drain
+    )
+
+    sst.run()
 
 
 if __name__ == '__main__':
--- src/motion_planners/sst_wrapper.cpp
+++ src/motion_planners/sst_wrapper.cpp
@@ -36,6 +36,7 @@ public:
         double sst_delta_drain
     )
     {
+        std::cout << random_seed << sst_delta_near << sst_delta_drain << std::endl;
         if (state_bounds_array.shape()[0] != control_bounds_array.shape()[0]) {
             throw std::runtime_error("State and control bounds arrays have to be equal size");
         }
@@ -54,13 +55,15 @@ public:
             is_circular_topology.push_back(is_circular_topology_array[i]);
         }
 
+        system_t* system = new point_t();
         planner.reset(
-            new sst_t(
-                    control_bounds, control_bounds,
-                    euclidian_distance(is_circular_topology),
-                    random_seed,
-                    sst_delta_near, sst_delta_drain)
+                new sst_t(
+                        state_bounds, control_bounds,
+                        euclidian_distance(is_circular_topology),
+                        random_seed,
+                        sst_delta_near, sst_delta_drain)
         );
+
     }
 
     void run() {
@@ -68,18 +71,12 @@ public:
         std::string planner_name = "sst";
         std::string system_name = "point";
 
-        int random_seed = 0;
         double start_state[] = {0., 0.};
         double goal_state[] = {9., 9.};
         double goal_radius = 0.5;
 
-        // The radius for BestNear (delta_n in the WAFR paper)
-        double sst_delta_near=.4;
-        // The radius for sparsification (delta_s in the WAFR paper)
-        double sst_delta_drain=.2;
-
-        std::string stopping_type = "time";
-        double stopping_check = 15;
+        std::string stopping_type = "iterations";
+        double stopping_check = 410000;
 
         std::string stats_type = "time";
         double stats_check_value = 0;
@@ -96,6 +93,10 @@ public:
         int min_time_steps = 20;
         int max_time_steps = 200;
         double integration_step = 0.002;
+
+        unsigned int random_seed = 0;
+        double sst_delta_near = 0.4;
+        double sst_delta_drain = 0.2;
 
 	    system_t* system = new point_t();
 
