af1e6f110aa14dd2b6a4db5dd38124ead653e225
Oleg Sinyavskiy <sinyavskiy@braincorporation.com>
2018-05-31 21:58:43 -0700

    refactoring

--- sparse_rrt/sandbox_sst.py
+++ sparse_rrt/sandbox_sst.py
@@ -24,6 +24,22 @@ system_classes = {
     'py_free_point': FreePoint
 }
 
+python_configs = {
+    'py_free_point_sst': dict(
+        system='py_free_point',
+        planner='sst',
+        start_state=np.array([0., 0.]),
+        goal_state=np.array([9., 9.]),
+        goal_radius=0.5,
+        random_seed=0,
+        sst_delta_near=0.4,
+        sst_delta_drain=0.2,
+        integration_step=0.002,
+        min_time_steps=20,
+        max_time_steps=200,
+        number_of_iterations=300000)
+}
+
 
 def load_standard_config(config_name):
     config_path = os.path.join(os.path.dirname(__file__), "../input")
@@ -43,7 +59,6 @@ def load_standard_config(config_name):
 def run_config(config):
     system = system_classes[config['system']]()
 
-    print(config)
     if config['planner'] == 'sst':
         planner = SST(
             state_bounds=system.get_state_bounds(),
@@ -69,7 +84,8 @@ def run_config(config):
     else:
         raise Exception("Uknown planner")
 
-    number_of_iterations = 300000
+    if 'number_of_iterations' not in config:
+        config['number_of_iterations'] = int(1e6)
 
     min_time_steps = int(config['min_time_steps'])
     max_time_steps = int(config['max_time_steps'])
@@ -78,7 +94,7 @@ def run_config(config):
     run_planning_experiment(
         planner,
         system,
-        number_of_iterations,
+        config['number_of_iterations'],
         min_time_steps,
         max_time_steps,
         integration_step
@@ -132,23 +148,6 @@ def run_planning_experiment(
     cv2.waitKey(-1)
 
 
-python_configs = {
-    'py_free_point_sst': dict(
-        system='py_free_point',
-        planner='sst',
-        start_state=np.array([0., 0.]),
-        goal_state=np.array([9., 9.]),
-        goal_radius=0.5,
-        random_seed=0,
-        sst_delta_near=0.4,
-        sst_delta_drain=0.2,
-        integration_step=0.002,
-        min_time_steps=20,
-        max_time_steps=200,
-        number_of_iterations=300000)
-}
-
-
 def find_config(name):
     if name in python_configs:
         return python_configs[name]
@@ -157,26 +156,3 @@ def find_config(name):
 
 if __name__ == '__main__':
     run_config(find_config('py_free_point_sst'))
-
-'''
-Took 0.0482540130615 to convert
-Time: 14.97s, Iterations: 103000, Planning time: 0.15ms Vis time: 80.99ms Nodes: 6524, Solution Quality: 1.49
-Took 0.0517320632935 to convert
-Time: 15.12s, Iterations: 104000, Planning time: 0.15ms Vis time: 84.69ms Nodes: 6548, Solution Quality: 1.49
-Took 0.05157995224 to convert
-Time: 15.28s, Iterations: 105000, Planning time: 0.17ms Vis time: 86.73ms Nodes: 6560, Solution Quality: 1.49
-Took 0.0501899719238 to convert
-Time: 15.44s, Iterations: 106000, Planning time: 0.16ms Vis time: 83.15ms Nodes: 6573, Solution Quality: 1.49
-Took 0.0520739555359 to convert
-Time: 15.60s, Iterations: 107000, Planning time: 0.16ms Vis time: 86.87ms Nodes: 6576, Solution Quality: 1.49
-Took 0.0505089759827 to convert
-Time: 15.77s, Iterations: 108000, Planning time: 0.17ms Vis time: 86.09ms Nodes: 6567, Solution Quality: 1.48
-Took 0.0507559776306 to convert
-Time: 15.91s, Iterations: 109000, Planning time: 0.14ms Vis time: 83.39ms Nodes: 6576, Solution Quality: 1.48
-Took 0.0552539825439 to convert
-Time: 16.09s, Iterations: 110000, Planning time: 0.18ms Vis time: 92.70ms Nodes: 6585, Solution Quality: 1.48
-Took 0.048957824707 to convert
-Time: 16.24s, Iterations: 111000, Planning time: 0.15ms Vis time: 82.75ms Nodes: 6591, Solution Quality: 1.48
-Took 0.0516459941864 to convert
-Time: 16.39s, Iterations: 112000, Planning time: 0.15ms Vis time: 85.87ms Nodes: 6612, Solution Quality: 1.48
-'''
